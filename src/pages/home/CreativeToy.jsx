6ac028cb06ce118ad3ace27f5bda03c1b0ed7cf0
Md Amran Hossen <mdamranhossen77@gmail.com>
2023-05-21 13:54:12 +0600

    fixed hight width of createtive card

--- src/pages/home/CreativeToy.jsx
+++ src/pages/home/CreativeToy.jsx
@@ -9,16 +9,16 @@ const CreativeToy = () => {
       }, []);
     return (
         <div className='container mx-auto mt-5 mb-10'>
-            <div className='relative px-2 h-[580px]'>
-                <div className='w-3/5'>
+            <div className='relative px-2'>
+                <div className='md:w-3/5 full'>
                 <img src={creativeToy} alt="" className='h-[400px] w-full rounded-lg' />
                 </div>
-                <div className='card shadow-lg px-3 py-4 w-3/6 absolute right-16 top-1/2 transform -translate-y-1/2 bg-white'
+                <div className='card shadow-lg px-3 py-4 w-full md:w-3/6 static md:absolute top-20 right-0 md:right-16 transform -translate-y-1/2 bg-white'
                 data-aos="fade-left"
                 >
-                    <h2 className='text-2xl font-bold'>Creative modern wooden toys for kids development</h2>
+                    <h2 className='text-2xl font-bold mb-5'>Creative modern wooden toys for kids development</h2>
                     <p>
-                    Introducing our collection of creative modern wooden toys, designed to foster holistic development in children. From open-ended play to problem-solving,ensuring a well-rounded and joyful learning experience.
+                    Discover our captivating collection of creative modern wooden toys, thoughtfully crafted to ignite kids imagination and promote holistic development. From open-ended play sets to educational puzzles, our eco-friendly toys inspire problem-solving, fine motor skills, and eco-consciousness for sustainable playtime adventures.
                     </p>
                 </div>
             </div>
